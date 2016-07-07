ac7a6986e181791116233ba753cddfd1aa384507
igorbashko <igor.littig@gmail.com>
2016-07-07 14:00:50 +0600

    Changed frame size

--- src/carmetric/CarMetric.java
+++ src/carmetric/CarMetric.java
@@ -37,7 +37,7 @@ public class CarMetric extends Application {
         StackPane root = new StackPane();
         root.getChildren().add(btn);
         
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(root, 700, 1250);
         
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
